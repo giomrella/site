654507b24a0b122641df0435eaa5f8921b2c194f
Giovanni Rella <rellag@nida.nih.gov>
2024-11-10 13:20:00 -0500

    hack to reroute to home from 404 on initial load

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -1,18 +1,14 @@
 use leptos::*;
-use leptos_router::{use_location, use_route, use_router};
-use logging::log;
+use leptos_router::{use_location, use_navigate};
 
 /// 404 Not Found Page
 #[component]
 pub fn NotFound() -> impl IntoView {
     let location = use_location();
-    log!("404 location: {:?}", location.pathname.get());
-    let route = use_route();
-    log!("404 route: {}, original_route: {}", route.path(), route.original_path());
-    let router = use_router();
-    log!("404 route: {}, 
-        base path: {},
-        base original path: {}
-        ", router.pathname().get(), router.base().path(), router.base().original_path());
+    let navigate = use_navigate();
+    if location.pathname.get() == "/site/" {
+        navigate("", Default::default());
+    }
+
     view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  }
 }
