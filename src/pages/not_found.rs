f71d7c58b0638083057a1750ab73f46cb9139be3
Giovanni Rella <rellag@nida.nih.gov>
2024-11-10 13:03:13 -0500

    adding more logging

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -1,5 +1,5 @@
 use leptos::*;
-use leptos_router::{use_location, use_route};
+use leptos_router::{use_location, use_route, use_router};
 use logging::log;
 
 /// 404 Not Found Page
@@ -9,5 +9,10 @@ pub fn NotFound() -> impl IntoView {
     log!("404 location: {:?}", location.pathname.get());
     let route = use_route();
     log!("404 route: {}, original_route: {}", route.path(), route.original_path());
+    let router = use_router();
+    log!("404 route: {}, 
+        base path: {},
+        base original path: {}
+        ", router.pathname().get(), router.base().path(), router.base().original_path());
     view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  }
 }
