a9b56fe5d0189673ee8de71ce53058c35f905d0e
Giovanni Rella <rellag@nida.nih.gov>
2024-11-10 12:23:41 -0500

    added logging to not_found page

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -1,12 +1,13 @@
 use leptos::*;
 use leptos_router::{use_location, use_route};
+use logging::log;
 
 /// 404 Not Found Page
 #[component]
 pub fn NotFound() -> impl IntoView {
     let location = use_location();
-    println!("location: {location:?}");
+    log!("location: {location:?}");
     let route = use_route();
-    println!("route: {route:?}");
+    log!("route: {route:?}");
     view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  }
 }
