53567959532b008222a15555af3a5d3ec10d1d10
Giovanni Rella <rellag@nida.nih.gov>
2024-11-10 12:55:41 -0500

    adding more routes to fix initial 404...

--- src/lib.rs
+++ src/lib.rs
@@ -34,8 +34,10 @@ pub fn App() -> impl IntoView {
 			  <A href="contact">"Contact"</A>
 			</nav>
             <Routes base="site".to_owned()>
+                <Route path="" view=Home/>
                 <Route path="/" view=Home/>
                 <Route path="site" view=Home/>
+                <Route path="site/" view=Home/>
                 <Route path="/site" view=Home/>
                 <Route path="/site/" view=Home/>
                 <Route path="/about" view=About/>
